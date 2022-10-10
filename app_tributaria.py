9f8196873b93a80059446e5fc2dc03ed83324a05
Estiven Garcia <miegarcialun@unal.edu.co>
2022-10-10 10:06:01 -0500

    feat: (@esgalu) test deploy cloud

--- app_tributaria.py
+++ app_tributaria.py
@@ -1,8 +1,12 @@
 import pandas as pd
-
 import streamlit as st
 
 uploaded_file = st.file_uploader("Arrastra o escoge un archivo")
+
+
+st.markdown("TITULO")
+
+st.sidebar.markdown("ARCHIVO")
 
 if uploaded_file is not None:
 
