e8c5379b0811d8e5bcff035793cfd2e17e14cb3b
Estiven Garcia <miegarcialun@unal.edu.co>
2022-10-10 10:08:34 -0500

    feat: (@esgalu) prepare structure

--- app_tributaria.py
+++ app_tributaria.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import streamlit as st
 
-uploaded_file = st.file_uploader("Arrastra o escoge un archivo")
 
 
 st.markdown("TITULO")
 
 st.sidebar.markdown("ARCHIVO")
 
+uploaded_file = st.sidebar.file_uploader("Arrastra o escoge un archivo")
+
 if uploaded_file is not None:
 
     dataframe = pd.read_excel(uploaded_file)
-    st.write(dataframe)
+    st.sidebar.write(dataframe)
