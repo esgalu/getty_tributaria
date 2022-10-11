756c804b490cd59969ed3078f658571afaf3c697
Estiven Garcia <miegarcialun@unal.edu.co>
2022-10-10 19:19:02 -0500

    feat: (@esgalu) add test functions

--- app_tributaria.py
+++ app_tributaria.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 
+from src.utils.calculate_date import count_days
 from src.utils.transform import clean_file
 
 st.markdown("TITULO")
@@ -13,4 +14,8 @@ if uploaded_file is not None:
 
     df = pd.read_excel(uploaded_file)
     df = clean_file(df)
+    days_in, days_out = count_days(df)
     st.sidebar.write(df)
+
+    st.markdown(f"fechas in {days_in}")
+    st.markdown(f"fechas out {days_out}")
