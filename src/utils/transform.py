10df26c4ac8deb0ce3c6e5af55d86b1fc1f9bc3a
Estiven Garcia <miegarcialun@unal.edu.co>
2022-10-10 19:18:13 -0500

    feat: (@esgalu) clean structure of code

--- src/utils/transform.py
+++ src/utils/transform.py
@@ -1,7 +1,13 @@
-def clean_file(df):
+dic_replace = {
+    'In/Out': {
+        'In': 1,
+        'Out': 0
+    }
+}
 
+
+def clean_file(df):
     df = df.dropna(subset=['In/Out'])
-    df = df.replace({'In/Out': {'In': 1, 'Out': 0}})
+    df = df.replace(dic_replace)
 
     return df
-
