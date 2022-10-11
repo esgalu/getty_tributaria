5d4fa83b027e05e0b37bffbf725d330ae3ecbb1d
Estiven Garcia <miegarcialun@unal.edu.co>
2022-10-10 19:17:48 -0500

    feat: (@esgalu) create function to calculate days

---
+++ src/utils/calculate_date.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from datetime import datetime
+
+
+def count_days(df):
+    in_out = df['In/Out'].tolist()
+
+    list_in_out = [in_out[i + 1] - in_out[i] for i in range(len(in_out) - 1) if
+                   in_out[i + 1] - in_out[i] != 0]
+    list_index = [index for index, i in enumerate(range(len(in_out) - 1)) if
+                  in_out[i + 1] - in_out[i] != 0]
+
+    tmp = df.loc[list_index].Fecha.tolist()
+
+    tmp.insert(0, df.Fecha.loc[0])
+    tmp.insert(len(tmp), pd.to_datetime(datetime.today()))
+
+    values = [(tmp[i + 1] - tmp[i]).days for i in range(len(tmp) - 1)]
+
+    list_in_out.insert(0, df['In/Out'].loc[0])
+
+    list_in = [index for index, _ in enumerate(list_in_out) if _ == 1]
+    list_out = [index for index, _ in enumerate(list_in_out) if _ == -1]
+
+    values_in = [value for index, value in enumerate(values) if
+                 index in list_in]
+    values_out = [value for index, value in enumerate(values) if
+                  index in list_out]
+
+    days_in = sum(values_in)
+    days_out = sum(values_out)
+
+    return days_in, days_out
